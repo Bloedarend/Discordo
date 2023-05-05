8879e0380c28e196534e37767590be83d71c59fc
Bloedarend <ofurylesso@gmail.com>
2023-05-05 19:43:47 +0200

    feat(broadcast): add broadcast command

---
+++ plugin/src/main/kotlin/dev/bloedarend/discordo/plugin/commands/Broadcast.kt
@@ -0,0 +1,25 @@
+package dev.bloedarend.discordo.plugin.commands
+
+import dev.bloedarend.discordo.plugin.Main
+import dev.bloedarend.discordo.plugin.utils.MessageUtil
+import org.bukkit.command.CommandSender
+import revxrsal.commands.annotation.Command
+import revxrsal.commands.annotation.Subcommand
+import revxrsal.commands.annotation.Usage
+import revxrsal.commands.bukkit.annotation.CommandPermission
+
+class Broadcast(private val plugin: Main) {
+
+    @Command("dco", "discordo")
+    @Subcommand("broadcast", "bc")
+    @CommandPermission("discordo.command.broadcast")
+    @Usage("dco broadcast")
+    fun onCommand(sender: CommandSender, message: String) {
+        plugin.discordo.sendImage(
+            MessageUtil.getMessage("commands.broadcast.format", sender, Pair("%message%", message)).replace('§', '&')
+        )
+
+        MessageUtil.sendMessage("commands.broadcast.message", sender)
+    }
+
+}
